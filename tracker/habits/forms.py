81cb29d9538a8a6b9d20a5527c63f2d81f8f9382
Vlad Karsten <vkarsten1111@icloud.com>
2024-12-10 15:46:31 +0700

    Feat: Complete habits list and settings templates

--- tracker/habits/forms.py
+++ tracker/habits/forms.py
@@ -1,4 +1,5 @@
 import django.forms
+import django.utils.translation
 
 import habits.models
 
@@ -7,9 +8,44 @@ __all__ = ()
 
 
 class BaseHabitForm(django.forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.visible_fields():
+            field.field.widget.attrs["class"] = "form-control"
+            if field.errors:
+                field.field.widget.attrs["class"] += " is-invalid"
+
     class Meta:
         model = habits.models.Habits
         fields = [
             habits.models.Habits.name.field.name,
             habits.models.Habits.count.field.name,
         ]
+        labels = {
+            habits.models.Habits.name.field.name: (
+                django.utils.translation.gettext_lazy("Имя привычки")
+            ),
+            habits.models.Habits.count.field.name: (
+                django.utils.translation.gettext_lazy("Цель")
+            ),
+        }
+        help_texts = {
+            habits.models.Habits.name.field.name: (
+                django.utils.translation.gettext_lazy("Обязательное поле")
+            ),
+            habits.models.Habits.count.field.name: (
+                django.utils.translation.gettext_lazy(
+                    "Сколько раз хочу выполнять привычку за день (от 1 до 24)",
+                )
+            ),
+        }
+        error_messages = {
+            habits.models.Habits.count.field.name: {
+                "min_value": django.utils.translation.gettext_lazy(
+                    "Значение должно быть не меньше 1.",
+                ),
+                "max_value": django.utils.translation.gettext_lazy(
+                    "Значение должно быть не больше 24.",
+                ),
+            },
+        }
