110c247070ed28defa4046bd673e8743e64a1380
Vlad Karsten <vkarsten1111@icloud.com>
2024-12-19 22:48:42 +0700

    Feat: Move DB to Postgres

--- tracker/tracker/settings.py
+++ tracker/tracker/settings.py
@@ -96,10 +96,14 @@ TEMPLATES = [
 WSGI_APPLICATION = "tracker.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+    }
 }
 
 
